fdc4de5be0e47eb43ca6d79e8baad6e9fa2d2e1a
chris13012 <christophecaillet@icloud.com>
2019-11-22 15:31:03 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,14 @@ import { DashboardComponent } from "./dashboard/dashboard.component";
 import { HttpClientModule } from "@angular/common/http";
 import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api";
 import { InMemoryDataService } from "./in-memory-data.service";
+import { HeroSearchComponent } from "./hero-search/hero-search.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
+    HeroSearchComponent,
     MessagesComponent,
     DashboardComponent
   ],
