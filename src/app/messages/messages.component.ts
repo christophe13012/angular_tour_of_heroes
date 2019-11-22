98d078b71225e713d4b2de9360c26f84e4057e6f
chris13012 <christophecaillet@icloud.com>
2019-11-22 15:33:25 +0100

    first commit

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -7,7 +7,7 @@ import { MessageService } from "./../message.service";
   styleUrls: ["./messages.component.css"]
 })
 export class MessagesComponent implements OnInit {
-  constructor(private messageService: MessageService) {}
+  constructor(public messageService: MessageService) {}
 
   ngOnInit() {}
 }
